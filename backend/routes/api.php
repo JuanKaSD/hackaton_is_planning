7c4eca3be86a0ad22732d2efcfc6ad81d9ee3562
Moises Santana <mjsdaw@gmail.com>
2025-03-31 22:59:17 +0100

    feat: add CORS config, update API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 Route::middleware([EnsureFrontendRequestsAreStateful::class, 'throttle:api'])->group(function () {
     Route::apiResource('users', UserController::class);
+    Route::put('users/{user}/edit', [UserController::class, 'update'])->name('users.edit');
 });
 
 Route::post('auth/register', [UserController::class, 'store']);
