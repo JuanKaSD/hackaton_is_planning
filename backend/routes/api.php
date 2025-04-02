0451a7dfe901ed3f0c3d5cea72e917adff9ea763
Moises Santana <mjsdaw@gmail.com>
2025-04-02 20:08:44 +0100

    hotfix: cleaning duplicate routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,8 +17,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('users/{user}/edit', [UserController::class, 'update'])->name('users.edit');
     Route::post('auth/logout', [UserController::class, 'logout']);
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.delete');
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
     Route::get('airlines', [AirlineController::class, 'index'])->name('airlines.index');
     Route::get('airlines/{airline}', [AirlineController::class, 'show'])->name('airlines.show');
     Route::post('airlines', [AirlineController::class, 'store'])->name('airlines.store');
