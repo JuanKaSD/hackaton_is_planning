6d408006d6039e80c2854587aff05712319a4ea4
JuanKa Santana <jsantado@gmail.com>
2025-04-02 19:49:59 +0100

    feat: small fixes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 "use client";
+
+import { MenuIcon } from 'lucide-react';
 import Link from "next/link";
 import styles from "@/styles/Navbar.module.css";
 import { usePathname, useRouter } from "next/navigation";
 import { authService } from "@/api/services/auth.service";
 import { useAuth } from "@/contexts/AuthContext";
+import { useState } from "react";
+
 
 export default function Navbar() {
   const pathname = usePathname();
   const router = useRouter();
   const { isAuthenticated, setAuth } = useAuth();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleLogout = () => {
     authService.logout();
@@ -24,14 +29,24 @@ export default function Navbar() {
         </Link>
         <div className={styles.links}>
           {!isAuthenticated ? (
-            <>
-              <Link href="/login" className={pathname === '/login' ? styles.active : ''}>
-                Login
-              </Link>
-              <Link href="/signup" className={pathname === '/signup' ? styles.active : ''}>
-                Sign Up
-              </Link>
-            </>
+            <div className={styles.dropdown}>
+              <button 
+                onClick={() => setDropdownOpen(!dropdownOpen)} 
+                className={styles.iconButton}
+              >
+                <MenuIcon className={styles.iconButton} />
+              </button>
+              {dropdownOpen && (
+                <div className={styles.dropdownMenu}>
+                  <Link href="/login" className={pathname === '/login' ? styles.active : ''}>
+                    Login
+                  </Link>
+                  <Link href="/signup" className={pathname === '/signup' ? styles.active : ''}>
+                    Sign Up
+                  </Link>
+                </div>
+              )}
+            </div>
           ) : (
             <button onClick={handleLogout} className={styles.logoutButton}>
               Logout
